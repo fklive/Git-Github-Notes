6d11dd142c4f78a45f69394e3f53b4e50c9395d7
Furkan Kasim <fkx1299@gmail.com>
2023-01-29 21:15:19 +0300

    My terminal command notes

---
+++ terminal_commands.cs
@@ -0,0 +1,15 @@
+
+#region Terminal Commands
+/*
+ls : Bulunduğumuz path'deki klasörleri gösterir.
+ls -la: Bulunduğumuz path'deki gizli klasörlerle beraber tüm klasörleri gösterir.
+cd (Change Directory) : İstenilen dizine gider.
+cd .. : Bir önceki bulunduğumuz dizine gider.
+clear : Terminal ekranını temizler.
+'Tab' tuşu : Yazdığımız komutları tamamlar -eğer doğru yazıyorsak-
+mkdir (Make Directory) : Bulunduğumuz dizine klasör oluşturur.
+touch : Bulunduğumuz dizine dosya oluşturur.
+rm (Remove) : Adı yazılan dosyayı siler.
+rm -rf :Adı yazılan klasörü siler. 
+*/
+#endregion
